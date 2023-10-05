0d664200f6435f9eccd9468d7f007c4d144dc500
moses <moses.kingi@moringaschool.com>
2023-10-05 08:39:07 +0300

    completed assigment

---
+++ index.js
@@ -0,0 +1,20 @@
+function divide(a,b) {
+    let a = 2000;
+    let b = 100;
+return (a / b);
+     }
+     divide();
+ 
+     // solution 2
+let x = 2;
+const square =  x => x**x ;
+
+ // solution 3
+
+let a = 3;
+let b = 4;
+const add = (a,b) => a+b ;
+
+
+    
+
